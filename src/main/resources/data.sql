4836a072b8272511dc8655a88159d1a284029682
BillyA15 <61017717+BillyA15@users.noreply.github.com>
2023-04-09 21:38:03 +0300

    schema update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,34 @@
 INSERT INTO professor VALUES
-                           (1,'Leslie','Andrews','leslie@luv2code.com'),
+                           (1,'Apostolos','Zarras','zarras@luv2code.com'),
                            (2,'Emma','Baumgarten','emma@luv2code.com'),
                            (3,'Avani','Gupta','avani@luv2code.com'),
                            (4,'Yuri','Petrov','yuri@luv2code.com'),
                            (5,'Juan','Vega','juan@luv2code.com');
 
 INSERT INTO thesis VALUES
-                            (1,'asd','Anddfhrews',1),
-                            (2,'dg','Baudhdmgarten',2),
-                            (3,'Adfgi','Gupgdfhta',3),
-                            (4,'Ydhdfhri','Pedhtrov',4),
-                            (5,'Jdhdhn','Vdhega',4);
+                            (1,'thesis1','Anddfhrews',1),
+                            (2,'thesis2','Baudhdmgarten',2),
+                            (3,'thesis3','Gupgdfhta',3),
+                            (4,'thesis4','Pedhtrov',4),
+                            (5,'thesis5','Vdhega',4);
+
+
+INSERT INTO student VALUES (1, 'Bob', 'Smith', 'bob@protonmail.com'),
+                            (2, 'Mary', 'Public', 'Mary@hotmail.com'),
+                            (3, 'John', 'Doe', 'Doe@uoi.gr');
+
+INSERT INTO subject VALUES (1, 'subject1', 'd1',1),
+                            (2, 'subject2', 'd2',2),
+                            (3, 'subject3', 'd3',3),
+                            (4, 'subject4', 'd4',4),
+                            (5, 'subject5', 'd5',4);
+
+INSERT INTO application VALUES (1,1),
+                                (1,2),
+                                (1,3),
+                                (2,1),
+                                (2,2),
+                                (2,3),
+                                (3,1),
+                                (3,2),
+                                (3,3);
